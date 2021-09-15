42a87c4b814e6499a65c3414943d8fbf8a9d14d5
Ukeachusim Loveth Ifunanya <ukeloveth247@gmail.com>
2021-09-15 17:10:19 +0100

    .

--- javascript/date-time.js
+++ javascript/date-time.js
@@ -9,13 +9,20 @@
 
 // No2: create a javascript application that will display how many days,months,
 //and years left for buhari to leave the office
- let countDown = new Date("may 29,2023");
+ let countDown = new Date("may 29,2023 12:30:00");
  let count = setInterval(function(){
      let presentDate = new Date().getTime();
      let difference = countDown - presentDate;
 
      let Days = Math.floor(difference/(1000*60*60*24));
      //console.log(Days);
-     let Months = Math.floor(difference/1000*60*60*24)/31);
-     console.log(Months)
- })
+     let Months = Math.floor(difference/ (1000 * 60 *60 * 24 * 30));
+     //console.log(Months)
+     let Years = Math.floor(difference/(1000 * 60 * 60 * 24 * 365));
+     //console.log(Years)
+     let Mintues = Math.floor(difference % (1000 * 60 * 60)/1000 * 60);
+     //console.log(Mintues);
+     let Seconds = Math.floor(difference%(1000*60)/1000);
+     console.log(Seconds);
+
+ });
